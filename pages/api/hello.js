b2145fd0995e400f7a615d7aceb0118a0354ca7e
docaotri123 <37148084+docaotri123@users.noreply.github.com>
2023-05-17 15:30:21 +0700

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,4 +1,8 @@
 export default function handler(req, res) {
     console.log("ABC ");
+    console.log(req.method);
+   console.log(req.body);
+   console.log(req.query);
+   console.log(req.headers);
     res.status(200).json({ text: 'Hello tri 3' });
   }
